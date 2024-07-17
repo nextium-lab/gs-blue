630de3c2e33a5e75970c99ffcea59904008714cb
nextium-lab <nextium.lab@gmail.com>
2024-07-16 22:54:45 -0300

    fix: add to nextium.com

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,9 @@
 export function Footer() {
     return(
         <section className="w-full bg-[#020817] py-3 border-t-[1px] border-white/10">
-            <div className="flex flex-col container py-3 text-center justify-center">
+            <div className="flex flex-col md:flex-row container py-3 text-center justify-between">
                 <span className="text-xs text-white">Todos os Direitos Reservados © GS Blue</span>
+                <span className="text-xs text-white">Desenvolvido por Nextium.com.br</span>
             </div>
         </section>
     )
