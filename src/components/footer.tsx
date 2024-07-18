828019a2a38d938436ffd0cfe7d7beac90512e61
nextium-lab <nextium.lab@gmail.com>
2024-07-17 21:38:49 -0300

    add corporation

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ export function Footer() {
     return(
         <section className="w-full bg-[#020817] py-3 border-t-[1px] border-white/10">
             <div className="flex flex-col md:flex-row container py-3 text-center justify-between">
-                <span className="text-xs text-white">Todos os Direitos Reservados © GS Blue</span>
+                <span className="text-xs text-white">Todos os Direitos Reservados © GS Blue Corporation</span>
                 <span className="text-xs text-white">Desenvolvido por nextium.com.br</span>
             </div>
         </section>
